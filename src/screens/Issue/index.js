8542775c11e3d56a6fd8e96802216dd97de81373
Darlene Costa <darl_mel@hotmail.com>
2022-10-14 06:32:25 -0300

    adicionado picker

--- src/screens/Issue/index.js
+++ src/screens/Issue/index.js
@@ -1,7 +1,19 @@
-import { Text } from "react-native";
+import { Text, View } from "react-native";
+import Title from "./../../components/Title";
+import Divider from "./../../components/Divider";
+import colors from "./../../styles/colors";
+import TextKeyValue from "./../../components/TextKeyValue";
 
 const Issue = () => {
-  return <Text>Issue</Text>;
+  return (
+    <>
+      <Title text="Fale Conosco" />;
+      <Divider colors={colors.primary.main} />
+      <TextKeyValue label="Id do pedido" text="#01231546" />
+      <Divider colors={colors.primary.main} />
+      <View style={styles.formContainer}></View>
+    </>
+  );
 };
 
 export default Issue;
