68f7ac91b14afb712e3c81461d265ba749d1046a
Darlene Costa <darl_mel@hotmail.com>
2022-10-18 05:56:48 -0300

    move combo box

--- src/screens/Complain/ComboBox/style.js
+++ src/screens/Complain/ComboBox/style.js
@@ -1,18 +1,18 @@
 import { StyleSheet } from "react-native";
-import colors from "../../styles/colors";
+import colors from "./../../../styles/colors";
 
-const styles = (error=false)=>
-StyleSheet.create({
-  combo: {
-    borderColor: error ? 'red': colors.primary.main,
-    borderWidth:1,
-    borderRadius: 25,
-    paddingHorizontal: 10
-  },
-  labelError: {
-    color: 'red',
-    fontSize: 12
-  },
-});
+const styles = (error = false) =>
+  StyleSheet.create({
+    combo: {
+      borderColor: error ? "red" : colors.primary.main,
+      borderWidth: 1,
+      borderRadius: 25,
+      paddingHorizontal: 10,
+    },
+    labelError: {
+      color: "red",
+      fontSize: 12,
+    },
+  });
 
 export default styles;
