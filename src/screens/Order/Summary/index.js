b11501af3e53f7830df4c5659d5d60bcefc52a74
Darlene Costa <darl_mel@hotmail.com>
2022-10-19 06:29:58 -0300

    melhorando a cobertura de testes

--- src/screens/Order/Summary/index.js
+++ src/screens/Order/Summary/index.js
@@ -3,25 +3,25 @@ import Subtitle from "../../../components/Subtitle";
 import utils from "../../../utils";
 import styles from "./style";
 
-const Summary = ({order}) => {
-
-    const detailItem = (key, value) => {
-        return (
-            <View style={styles.wrapper}>
-                <Text style={styles.key}>{key}</Text>
-                <Text style={styles.value}>{value}</Text>
-            </View>
-        )
-    }
-
+const Summary = ({ order }) => {
+  const detailItem = (key, value) => {
     return (
-    <View >
-        <Subtitle text="RESUMO DO PEDIDO" />
-            {detailItem("data",  utils.formatDate(order.dataCreated))}
-            {detailItem("valor",  utils.formatMoney(order.value))}            
-            {detailItem("status", order.status)}
-            {detailItem("Tipo do Frete", order.delivery.type)}
-    </View>)
-}
+      <View style={styles.wrapper}>
+        <Text style={styles.key}>{key}</Text>
+        <Text style={styles.value}>{value}</Text>
+      </View>
+    );
+  };
 
-export default Summary
+  return (
+    <View>
+      <Subtitle text="RESUMO DO PEDIDO" />
+      {detailItem("data", utils.formatDate(order.dateCreated))}
+      {detailItem("valor", utils.formatMoney(order.value))}
+      {detailItem("status", order.status)}
+      {detailItem("Tipo do Frete", order.delivery.type)}
+    </View>
+  );
+};
+
+export default Summary;
