f52d600720f18df39d9f20c779a6548c7d26fbc4
darlenecosta <darl_mel@hotmail.com>
2022-10-18 21:19:30 -0300

    remove modal não usada

--- App.js
+++ App.js
@@ -1,16 +1,13 @@
 import { SafeAreaView, StyleSheet } from "react-native";
-import { NavigationContainer } from "@react-navigation/native";
 import Header from "./src/components/Header/index";
 import Router from "./src/routers";
 
 export default function App() {
   return (
-    <NavigationContainer>
       <SafeAreaView style={styles.container}>
         <Header />
         <Router />
       </SafeAreaView>
-    </NavigationContainer>
   );
 }
 
